dc8d0d948f111d5d6371479b6093b91d386d7456
Haris Nasution <haris.rahman@edi-indonesia.co.id>
2023-03-22 13:56:31 +0700

    change upload to add button

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts/app')
 
 @section('style')
-<link href="{{ asset('css/list.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/list.css') }}" rel="stylesheet" />
 @endsection
 @section('title', 'ONEPLAY - Students Management') @section('content')
 <div class="container">
     @if (session('success'))
-    <div class="row justify-center">
-        <div class="col-xs-12 col-md-8">
-            <div class="alert alert-success alert-dismissible fade show" role="alert" id="myAlert">
-                {{ session('success') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"
-                    onclick="closeAlert()">
-                    <i class="fa fa-times" aria-hidden="true"></i>
-                </button>
+        <div class="row justify-center">
+            <div class="col-xs-12 col-md-8">
+                <div class="alert alert-success alert-dismissible fade show" role="alert" id="myAlert">
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"
+                        onclick="closeAlert()">
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
     @endif
     <h1>List of Students</h1>
     <div class="row mb-5">
@@ -47,39 +47,41 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @if(count($mahasiswa) < 1) <tr>
-                        <td colspan="12" class="text-center">
-                            Data not found.
-                        </td>
-                        </tr>
-                        @else
-                        @foreach ($mahasiswa as $index=> $data)
+                    @if (count($mahasiswa) < 1)
                         <tr>
-                            <td>{{$index + 1}}</td>
-                            <td>{{$data->nisn}}</td>
-                            <td align="left">{{$data->nama}}</td>
-                            <td>{{$data->quiz}}</td>
-                            <td>{{$data->tugas}}</td>
-                            <td>{{$data->absen}}</td>
-                            <td>{{$data->praktek}}</td>
-                            <td>{{$data->uas}}</td>
-                            <td>{{$data->getTotalScore()}}</td>
-                            <td>{{$data->getAverageScore()}}</td>
-                            <td><b>{{$data->getGrade()}}</b></td>
-                            <td>
-                                <form action="{{ route('mahasiswa.destroy', $data->id) }}" method="POST">
-                                    <a class="btn btn-primary" href="{{ route('mahasiswa.edit', $data->id) }}">Edit</a>
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                </form>
+                            <td colspan="12" class="text-center">
+                                Data not found.
                             </td>
                         </tr>
+                    @else
+                        @foreach ($mahasiswa as $index => $data)
+                            <tr>
+                                <td>{{ $index + 1 }}</td>
+                                <td>{{ $data->nisn }}</td>
+                                <td align="left">{{ $data->nama }}</td>
+                                <td>{{ $data->quiz }}</td>
+                                <td>{{ $data->tugas }}</td>
+                                <td>{{ $data->absen }}</td>
+                                <td>{{ $data->praktek }}</td>
+                                <td>{{ $data->uas }}</td>
+                                <td>{{ $data->getTotalScore() }}</td>
+                                <td>{{ $data->getAverageScore() }}</td>
+                                <td><b>{{ $data->getGrade() }}</b></td>
+                                <td>
+                                    <form action="{{ route('mahasiswa.destroy', $data->id) }}" method="POST">
+                                        <a class="btn btn-primary"
+                                            href="{{ route('mahasiswa.edit', $data->id) }}">Edit</a>
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
                         @endforeach
-                        @endif
+                    @endif
                 </tbody>
             </table>
-            <a href="{{ route('mahasiswa.create') }}" class="btn btn-primary">+ Upload New Data</a>
+            <a href="{{ route('mahasiswa.create') }}" class="btn btn-primary">+ Add New Data</a>
         </div>
     </div>
 </div>
